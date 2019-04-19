644f47143cb5df40a2c000c6fb49e3043a693c7b
Sushil khanchi <sushilkhanchi97@gmail.com>
2019-04-20 00:56:07 +0530

    updated setup script after structure change

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 setup(
     name="buddy",
     version="0.1",
-    py_modules = ['launch','chain','util'],
+    packages=['src'],
+    py_modules = ['src.launch','src.chain','src.util'],
     install_requires=[
         'click',
     ],
     # console script entrypont
     entry_points='''
         [console_scripts]
-        buddy=launch:work
+        buddy=src.launch:work
     ''',
 )
